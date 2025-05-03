3085f565700b33cbba1026e260b9f9960c19751c
Wilberth Loría <stwilberth@gmail.com>
2025-05-03 12:57:19 -0600

    r

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::get('/', \App\Livewire\ProductFilter::class);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
