aa2060293b113a97666f9a67cdde039f68a157b5
Wilberth Loría <stwilberth@gmail.com>
2025-05-03 13:03:39 -0600

    ttt

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,9 +8,14 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Styles -->
+    <style>
+        [x-cloak] { display: none !important; }
+    </style>
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+    @livewireStyles
+    @livewireScripts
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
         <!-- Page Content -->
@@ -18,5 +23,7 @@
             {{ $slot }}
         </main>
     </div>
+    @stack('modals')
+    @stack('scripts')
 </body>
 </html>
